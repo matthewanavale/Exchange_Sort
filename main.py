adbcb6697bbf66f60c742b55ba05c1f5a946faf9
matthewanavale <navalematthew12@gmail.com>
2023-07-08 15:46:48 +0800

    push

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+class MainWidget(Widget):
+    pass
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+Builder.load_file('main.kv')
+class MyApp(App):
+    def build(self):
+        Window.clearcolor = (0, 0, 0, 1)
+        Window.size = (1080, 720)
+        self.title = 'Exchange Sort'
+        return MainWidget()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    MyApp().run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
